96c0150d2e517893ec889b971c206295842f07bc
FranLsz <franlsz95develop@gmail.com>
2016-05-25 17:36:02 +0200

    Base

---
+++ Angular2rc/app/components/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: BASE_URL + '/templates/app.template.html'
+})
+
+export class AppComponent {
+    public unaVar: string;
+
+    constructor() {
+        this.unaVar = "yep";
+    }
+}
